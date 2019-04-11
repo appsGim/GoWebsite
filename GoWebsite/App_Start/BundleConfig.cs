ebf684c5b084a26c4d0ebf997d1bef8162cfdeaa
Eliran Ben lulu <eliran.b@moblin.co.il>
2019-04-11 16:35:30 +0300

    first

--- GoWebsite/App_Start/BundleConfig.cs
+++ GoWebsite/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace GoWebsite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                "~/Scripts/plugins/particleground/jquery.particleground.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ GoWebsite/Scripts/plugins/particleground/demo.js
@@ -0,0 +1,26 @@
+﻿/**
+* Particleground demo
+* @author Jonathan Nicol - @mrjnicol
+*/
+
+//document.addEventListener('DOMContentLoaded', function () {
+//    particleground(document.getElementById('particles'), {
+//        dotColor: '#5cbdaa',
+//        lineColor: '#5cbdaa'
+//    });
+//    var intro = document.getElementById('intro');
+//    intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+//}, false);
+
+
+
+ //jQuery plugin example:
+$(document).ready(function() {
+    $('#intro').particleground({
+        dotColor: '#ffffff',
+        lineColor: '#ffffff'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 5)
+  });
+});
