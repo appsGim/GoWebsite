71ed1b217f548428559d8dbc6a51b2bd40634fe4
Eliran Ben lulu <eliran.b@moblin.co.il>
2019-04-15 15:03:05 +0300

    Go Moblin One Page

--- GoWebsite/Scripts/plugins/particleground/demo.js
+++ GoWebsite/Scripts/plugins/particleground/demo.js
@@ -3,24 +3,25 @@
 * @author Jonathan Nicol - @mrjnicol
 */
 
-//document.addEventListener('DOMContentLoaded', function () {
-//    particleground(document.getElementById('particles'), {
-//        dotColor: '#5cbdaa',
-//        lineColor: '#5cbdaa'
-//    });
-//    var intro = document.getElementById('intro');
-//    intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-//}, false);
 
+document.addEventListener('DOMContentLoaded', function () {
+    particleground(document.getElementById('particles'), {
+        dotColor: '#ffffff',
+        lineColor: '#ffffff'
+    });
+    var intro = document.getElementById('intro');
+    intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
 
 
  //jQuery plugin example:
-$(document).ready(function() {
-    $('#intro').particleground({
-        dotColor: '#ffffff',
-        lineColor: '#ffffff'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 5)
-  });
-});
+//$(document).ready(function() {
+//    $('#intro').particleground({
+//    dotColor: '#ffffff',
+//        lineColor: '#ffffff'   
+      
+//  });
+//  $('.intro').css({
+//    'margin-top': -($('.intro').height() / 10)
+//  });
+//});
